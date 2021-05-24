a7dcd0effe43b75425622591a32bb4944fc06a3a
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-05-24 18:56:18 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
+  let elems = document.querySelectorAll('.sidenav');
+  let instances = M.Sidenav.init(elems, {});
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, {});
+  let elems = document.querySelectorAll('.collapsible');
+  let instances = M.Collapsible.init(elems, {});
+});
+
+let expandButtonsCollection = document.getElementsByClassName('expand-button');
+let expandButtons = [].slice.call(document.getElementsByClassName('expand-button'))
+expandButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.innerHTML === 'expand_more') button.innerHTML = 'expand_less';
+    else button.innerHTML = 'expand_more';
+  })
 });
