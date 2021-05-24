e21809e61d204b21ac175fe197c7ea9b6f45c20a
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-05-25 02:33:00 +0800

    update

--- js/index.js
+++ js/index.js
@@ -8,11 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
   let instances = M.Collapsible.init(elems, {});
 });
 
-let expandButtonsCollection = document.getElementsByClassName('expand-button');
-let expandButtons = [].slice.call(document.getElementsByClassName('expand-button'))
-expandButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (button.innerHTML === 'expand_more') button.innerHTML = 'expand_less';
-    else button.innerHTML = 'expand_more';
-  })
+// let expandButtonsCollection = document.getElementsByClassName('expand-button');
+// let expandButtons = [].slice.call(document.getElementsByClassName('expand-button'))
+// expandButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     if (button.innerHTML === 'expand_more') button.innerHTML = 'expand_less';
+//     else button.innerHTML = 'expand_more';
+//   })
+// });
+
+let collapsibleHeaders = [].slice.call(document.getElementsByClassName('collapsible-header'));
+collapsibleHeaders.forEach(elem => {
+  let expandButton = elem.querySelector('.expand-button');
+  elem.addEventListener('click', () => {
+    if (expandButton.innerHTML === 'expand_more') expandButton.innerHTML = 'expand_less';
+    else expandButton.innerHTML = 'expand_more';
+  });
 });
