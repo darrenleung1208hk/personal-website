4028ae4a0145ecb2b54cd7f7fbdaf3a252bd653c
darrenleung1208hk <leungyatlong1208@gmail.com>
2021-05-25 02:33:10 +0800

    update

--- js/index.js
+++ js/index.js
@@ -8,15 +8,6 @@ document.addEventListener('DOMContentLoaded', function () {
   let instances = M.Collapsible.init(elems, {});
 });
 
-// let expandButtonsCollection = document.getElementsByClassName('expand-button');
-// let expandButtons = [].slice.call(document.getElementsByClassName('expand-button'))
-// expandButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     if (button.innerHTML === 'expand_more') button.innerHTML = 'expand_less';
-//     else button.innerHTML = 'expand_more';
-//   })
-// });
-
 let collapsibleHeaders = [].slice.call(document.getElementsByClassName('collapsible-header'));
 collapsibleHeaders.forEach(elem => {
   let expandButton = elem.querySelector('.expand-button');
